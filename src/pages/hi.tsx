ac2964e93573243ed3804592a707f1896908a717
Karthikeya Kona <konakarthik12@gmail.com>
2023-03-13 16:53:40 -0400

    frontend first commit

---
+++ src/pages/hi.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return <div className="hero min-h-screen bg-base-200">
+    <div className="hero-content text-center">
+      <div className="max-w-md">
+        <h1 className="text-5xl font-bold">Hello there</h1>
+        <p className="py-6">Welcome to the future of coding</p>
+        <button className="btn btn-primary">Get Started</button>
+      </div>
+    </div>
+  </div>;
+}
